eca6738635b8921af01ef61e9166e049100c2803
Amit Kumar <kumaramitrou@gmail.com>
2019-11-19 17:04:03 +0530

    update

--- project/src/com/ohdgh/db/DatabaseCredentials.java
+++ project/src/com/ohdgh/db/DatabaseCredentials.java
@@ -4,7 +4,7 @@ public class DatabaseCredentials {
     static String hostName = "onlinehelpdesk.database.windows.net"; // update me
     static String dbName = "Development"; // update me
     static String user = "ohdgh"; // update me
-    static String password = "Welcome@123"; // update me
+    static String password = ""; // update me
     static String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
         + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
     static String driver ="com.microsoft.sqlserver.jdbc.SQLServerDriver";
